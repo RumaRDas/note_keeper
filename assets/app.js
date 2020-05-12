82ed72ba5499bc3018b3f032adf7e71aa92689d3
Ruma Das <rumaadl16@gmail.com>
2020-05-13 01:09:05 +0930

    Include all file

--- assets/app.js
+++ assets/app.js
@@ -26,7 +26,8 @@ addBtn.addEventListener("click", function (e) {
     console.log(notesObj);
     showNotes();
 });
-//function for inserting and showing data in html div
+
+//function for inserting and showing element note from local storage
 function showNotes() {
     let notes = localStorage.getItem("notes");
     //if local storage is null 
@@ -48,7 +49,7 @@ function showNotes() {
         <div class="card-body">
             <h5 class="card-title">Note ${index + 1}</h5>
             <p class="card-text"> ${element}</p>
-            <button class="btn btn-info">Delete Note</button>
+            <button id="${index}" onclick = "deleteNote(this.id)" class="btn btn-info">Delete Note</button>
         </div>
     </div>
         `
@@ -61,7 +62,50 @@ function showNotes() {
 
     } else {
         // if notes length is 0
- notesElm.innerHTML = `<h2 class="text-info">You can save your Note Here</h2>`
+        notesElm.innerHTML = `<h2 class="text-info">You can save your Note Here</h2>`
     }
-
 }
+
+// Function to Delet Note
+function deleteNote(index) {
+    console.log('I am deleting', index);
+    let notes = localStorage.getItem("notes");
+    //if local storage is null 
+    if (notes == null) {
+        //notes object array
+        notesObj = [];
+    }
+    else {
+
+        notesObj = JSON.parse(notes);
+    }
+    // deleting 1 element from local object array
+    notesObj.splice(index, 1);
+    // after deleting updating localtorage  string element
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
+
+// For search notes by input
+let search = document.getElementById("searchTxt");
+// adding event listener
+search.addEventListener("input" ,function(){
+   // alltype of input lowercase or upper case
+    let inputVal = search.value;
+
+   // console.log('Input for ntes', inputVal);
+    let noteCards = document.getElementsByClassName('noteCard');
+
+    Array.from(noteCards).forEach(function(element){
+       
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+
+    })
+
+});
